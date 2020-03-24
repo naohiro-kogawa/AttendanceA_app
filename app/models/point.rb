08343b8c46fc3eb1e4ccd6b15d698cb161fb6e36
EC2 Default User <ec2-user@ip-172-31-9-171.us-east-2.compute.internal>
2020-03-24 08:08:42 +0000

    管理者画面：拠点一覧：カラムバリデーション修正:拠点番号セレクト修正

--- app/models/point.rb
+++ app/models/point.rb
@@ -1,6 +1,6 @@
 class Point < ApplicationRecord
     
-  validates :point_name, presence: true ,length: { maximum: 30 }  
-  validates :point_number, presence: true , numericality:{ less_than_or_equal_to: 100 } #指定された値と等しいか、あるいは小さいか
+  validates :point_name, presence: true ,length: { in: 1..30 }  
+  validates :point_number, presence: true
   validates :attendance_type, presence: true    
 end
