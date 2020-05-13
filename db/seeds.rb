73be2e32f30c82da3332070ef3511701867a947c
EC2 Default User <ec2-user@ip-172-31-9-171.us-east-2.compute.internal>
2020-05-13 07:27:37 +0000

    seedファイル作成、管理者の編集、上長AB

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,32 @@
 # coding: utf-8
 
 User.create!(name: "管理者",
-             email: "sample@email.com",
+             email: "sample3762@email.com",
              password: "password",
              password_confirmation: "password",
+             department: "本社",
+             employee_number: 3762,
+             uid: 762,
              admin: true)
+             
+ User.create!(name:  "上長A",
+             email: "sample2597@email.com",
+             password:              "password",
+             password_confirmation: "password",
+             department: "本社",
+             employee_number: 2597,
+             uid: 597,
+             superior: true)
+
+  User.create!(name:  "上長B",
+             email: "sample3470@email.com",
+             password:              "password",
+             password_confirmation: "password",
+             department: "本社",
+             employee_number: 3470,
+             uid: 470,
+             superior: true)     
+             
 
 60.times do |n|
   name  = Faker::Name.name
@@ -12,6 +34,9 @@ User.create!(name: "管理者",
   password = "password"
   User.create!(name: name,
                email: email,
+               department: "本社",
+               employee_number: n+1,
+               uid: n+1,
                password: password,
                password_confirmation: password)
 end
